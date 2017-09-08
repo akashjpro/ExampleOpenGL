11d31f5449b65348cf130934127d22d32a6ec735
ISB-VIETNAM\tmha <tmha@vn.isb.co.jp>
2017-09-08 17:38:20 +0700

    commit

---
+++ app/src/main/java/com/example/tmha/exampleopengl/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.tmha.exampleopengl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tmha.exampleopengl.lesson1.TriangleColorActivity;
+import com.example.tmha.exampleopengl.lesson2.LessonTwoActivity;
+import com.example.tmha.exampleopengl.lesson3.LessonThreeActivity;
+import com.example.tmha.exampleopengl.lesson4.LessonFourActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lessonOneClick(View view) {
+        startActivity(new Intent(this, TriangleColorActivity.class));
+    }
+
+    public void lessonTwoClick(View view) {
+        startActivity(new Intent(this, LessonTwoActivity.class));
+    }
+
+    public void lessonThreeClick(View view) {
+        startActivity(new Intent(this, LessonThreeActivity.class));
+    }
+
+    public void lessonFourClick(View view) {
+        startActivity(new Intent(this, LessonFourActivity.class));
+    }
+}
